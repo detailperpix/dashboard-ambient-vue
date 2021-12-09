a42c006588dba72e184d2c05380be1b762a2108c
Wirman <wirmantonochong@gmail.com>
2021-12-09 11:29:57 +0800

    Change path to root and remove vuetify header

--- src/router/index.js
+++ src/router/index.js
@@ -7,16 +7,8 @@ Vue.use(VueRouter);
 const routes = [
     {
         path: '/',
-        name: 'Home',
-        component: Home,
-    },
-    {
-        path: '/ambient-chart',
         name: 'Ambient Chart',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/AmbientChart.vue'),
+        component: () => import('../views/AmbientChart.vue'),
     },
 ];
 
